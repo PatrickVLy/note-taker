8a813ddf77daffad669ea09f19016a0ca0be550e
Patrick Ly <patrickly72@gmail.com>
2021-04-21 20:44:12 -0700

    created routes for notes/index, and api

--- server.js
+++ server.js
@@ -11,3 +11,19 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({extended: true}));
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(mainDir, "index.html"));
+});
