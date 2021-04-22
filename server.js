d4b5d11909a392ba40475c9eeba8125ef15bfeaa
Patrick Ly <patrickly72@gmail.com>
2021-04-21 20:40:50 -0700

    added application middleware

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const fs = require("fs");
 const mainDir = path.join(__dirname, "/public");
 const port = 3000;
 const app = express();
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true}));
+
