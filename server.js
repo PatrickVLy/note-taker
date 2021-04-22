4ecaee6316339423301c52dead85a9505c562cc7
Patrick Ly <patrickly72@gmail.com>
2021-04-21 20:23:37 -0700

    added variables for express function, port and mainDir path method

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const mainDir = path.join(__dirname, "/public");
+const port = 3000;
+const app = express();
