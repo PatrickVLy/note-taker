3a40382e6cb8c1ab9d9d474c2650cb13e0d737d0
Patrick Ly <patrickly72@gmail.com>
2021-04-21 21:33:15 -0700

    pushed files to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const mainDir = path.join(__dirname, "/public");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
@@ -70,6 +70,6 @@ app.delete("/api/notes/:id", function(req, res) {
     res.json(savedNotes);
 })
 
-app.listen(port, function() {
-    console.log(`application listening on port ${port}`);
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
 })
